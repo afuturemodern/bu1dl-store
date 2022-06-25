2529644bf15bb998b0900d7ffc0bf8bf59beab08
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-06-26 01:36:45 +0200

    update img

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -3,7 +3,7 @@ if( !window.fmbs ) {
 }
 
 window.fmbs.galleryMarkup = `<div class="fmbs-gallery-grid-item">
-        <img class="fmbs-gallery-grid-item__img" src="http://ipfs.io/ipfs/QmdLUGQdXr5npDKaY5ju4Eu2Z5XbZhk1oBC5v7SNsDGZAy/nft.png"/>
+        <img class="fmbs-gallery-grid-item__img" src="https://i.imgur.com/eL5JPhi.jpg"/>
         <h4 class="fmbs-gallery-grid-item__text">Edition #001  100thousand  KirbLaGoop Plush</h4>
         <p class="fmbs-gallery-grid-item__description">EDITION 001 of 100 Physical 100 Thousand Kirblagoop Plush <br>100 Thousand Plush Collaboration Flexicoworldwide Marcy Mane and Kirblagoop 100 Thousand. Plush Created based off design by Aidan Capristo. <br><br>Physical Plush 12 Inch<br>with MP3 Playable Sounds</p>
         <a class="fmbs-gallery-grid-item__more-link" href="javascript://">[more about the item]</a>
